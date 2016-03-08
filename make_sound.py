55705f4e543ed149abfa329a60fd490016c62681
Alan R <alan.rominger@gmail.com>
2016-03-07 20:28:36 -0500

    stop it from making newlines

--- make_sound.py
+++ make_sound.py
@@ -1,9 +1,11 @@
 import random
 import time
 from math import exp
+import sys
 
 for i in range(100):
   r = random.randint(0, 100)
   time.sleep(100./r)
-  print('\a')
+  sys.stdout.write('\a')
+  sys.stdout.flush()
 
